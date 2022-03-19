120cb244373da9419e24ac8a416f4181d2e7bda9
Robert Tanase <robert.tanase97@gmail.com>
2022-03-19 20:32:58 +0200

    Extract system under task under a factory method

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import EssentialFeed
+
 class CodableFeedStore {
 
     private struct Cache: Codable {
@@ -74,7 +75,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache retrieve")
 
         sut.retrieve { result in
@@ -92,7 +93,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for cache retrieve")
 
         sut.retrieve { firstResult in
@@ -112,7 +113,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
 
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueImageFeed().local
         let timestamp = Date()
         let exp = expectation(description: "Wait for cache retrieve")
@@ -135,5 +136,11 @@ class CodableFeedStoreTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 1.0)
+    }
+
+    // MARK: Helpers
+
+    private func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
 }
