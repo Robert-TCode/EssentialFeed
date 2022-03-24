7dd0a0910100a6710621c4727607a37fd2b9a31a
Robert Tanase <robert.tanase97@gmail.com>
2022-03-24 23:43:19 +0200

    Ensures insert overrides feed inserted on another instance

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        try setupEmptyStoreState()
+    }
+
+    override func tearDownWithError() throws {
+        try undoStoreSideEffects()
+
+        try super.tearDownWithError()
+    }
+
     func test_retrieve_deliversEmptyOnEmptyCache() throws {
         let sut = try makeSUT()
 
@@ -27,6 +39,20 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         expect(storeToLoad, toRetrieve: .found(feed: feed.local, timestamp: timestamp))
     }
 
+    func test_insert_overridesFeedInsertedOnAnotherInstance() throws {
+        let storeToInsert = try makeSUT()
+        let storeToOverride = try makeSUT()
+        let storeToLoad = try makeSUT()
+
+        insert((uniqueImageFeed().local, Date()), to: storeToInsert)
+
+        let latestFeed = uniqueImageFeed()
+        let latestTimestamp = Date()
+        insert((latestFeed.local, latestTimestamp), to: storeToOverride)
+
+        expect(storeToLoad, toRetrieve: .found(feed: latestFeed.local, timestamp: latestTimestamp))
+    }
+
     // - MARK: Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) throws -> FeedStore {
@@ -35,6 +61,18 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         return sut
     }
 
+    private func setupEmptyStoreState() throws {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() throws {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
     private func testSpecificStoreURL() -> URL {
         cachesDirectory().appendingPathComponent("\(type(of: self)).store")
     }
