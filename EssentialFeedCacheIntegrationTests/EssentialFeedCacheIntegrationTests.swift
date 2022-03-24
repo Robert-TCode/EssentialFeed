6d685c881dd534ac443242c73d8613808f7fb0ac
Robert Tanase <robert.tanase97@gmail.com>
2022-03-25 00:15:32 +0200

    Clean up and undo all cache side-effects on `setUp` and `tearDown` to avoid sharing state between tests

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -10,6 +10,18 @@ import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+
+        setupEmptyStoreState()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        undoStoreSideEffects()
+    }
+
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
 
@@ -38,11 +50,23 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         return sut
     }
 
-    private func testSpecificStoreURL() -> URL {
-            return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
-        }
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
 
-        private func cachesDirectory() -> URL {
-            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        }
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
