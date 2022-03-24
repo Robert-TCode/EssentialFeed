fd143253c39126eb37b91121e64a9db616ef2d03
Robert Tanase <robert.tanase97@gmail.com>
2022-03-24 22:54:53 +0200

    Add CoreData implementation for `FeedStore`

---
+++ EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStoreModels/ManagedFeedImage.swift
@@ -0,0 +1,41 @@
+//
+//  ManagedFeedImage+CoreDataClass.swift
+//  FeedStoreChallenge
+//
+//  Created by TCode on 21/3/22.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+final class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+	static func images(from localFeed: [LocalFeedImage],
+	                   in context: NSManagedObjectContext) -> NSOrderedSet {
+		return NSOrderedSet(array: localFeed.map { local in
+			let managedFeedImage = ManagedFeedImage(context: context)
+			managedFeedImage.id = local.id
+			managedFeedImage.location = local.location
+			managedFeedImage.imageDescription = local.description
+			managedFeedImage.url = local.URL
+			return managedFeedImage
+		})
+	}
+
+	func asLocal() -> LocalFeedImage {
+		LocalFeedImage(id: self.id,
+		               description: self.imageDescription,
+		               location: self.location,
+                       URL: self.url)
+	}
+}
