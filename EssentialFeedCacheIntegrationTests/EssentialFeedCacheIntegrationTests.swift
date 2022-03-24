8692e2623499d9c35615b2495ce99b73e3515369
Robert Tanase <robert.tanase97@gmail.com>
2022-03-24 23:38:08 +0200

    Ensure retrieve delivers empty on empty cache when being used with a non-dev store

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -6,7 +6,29 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
+    func test_retrieve_deliversEmptyOnEmptyCache() throws {
+        let sut = try makeSUT()
+
+        expect(sut, toRetrieve: .empty)
+    }
+
+    // - MARK: Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) throws -> FeedStore {
+        let sut = try CoreDataFeedStore(storeURL: testSpecificStoreURL())
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func testSpecificStoreURL() -> URL {
+        cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+
+    private func cachesDirectory() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
