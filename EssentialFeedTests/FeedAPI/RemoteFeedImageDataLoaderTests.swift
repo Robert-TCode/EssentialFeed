1c10df43cb78cc716de36ce7be99043fa3726413
Robert Tanase <robert.tanase97@gmail.com>
2022-05-24 21:17:45 +0300

    `RemoteFeedImageDataLoader` requests data from URL twice on calling `loadImageDataFromURL` twice

--- EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -10,35 +10,45 @@ import EssentialFeed
 
 class RemoteFeedImageDataLoader {
     private let client: HTTPClient
-    
+
     init(client: HTTPClient) {
         self.client = client
     }
-    
+
     func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
         client.get(from: url) { _ in }
     }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
-    
+
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
-        
+
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-    
+
     func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
         
-        XCTAssertEqual(client.requestedURLs, [url])
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     // MARK: Helpers
-    
+
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
@@ -46,10 +56,10 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
     }
-    
+
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        
+
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             requestedURLs.append(url)
         }
