500feab16996d4618aef0ea2ea4a48c6f268f072
Robert Tanase <robert.tanase97@gmail.com>
2022-06-10 15:55:23 +0300

    Test `ImageCommentsMapper` in isolation

--- EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -22,17 +22,17 @@ class FeedItemsMapperTests: XCTestCase {
     }
 
     func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() throws {
-        let invalidJSON = Data("invalid-data".utf8)
+        let invalidJSONData = Data("invalid-data".utf8)
 
         XCTAssertThrowsError(
-            try FeedItemsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
+            try FeedItemsMapper.map(invalidJSONData, from: HTTPURLResponse(statusCode: 200))
         )
     }
 
     func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
-        let emptyListJSON = makeItemsJSON([])
+        let emptyListJSONData = makeItemsJSON([])
 
-        let result = try FeedItemsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
+        let result = try FeedItemsMapper.map(emptyListJSONData, from: HTTPURLResponse(statusCode: 200))
 
         XCTAssertEqual(result, [])
     }
@@ -44,18 +44,14 @@ class FeedItemsMapperTests: XCTestCase {
                              description: "a description",
                              location: "a location",
                              imageURL: URL(string: "https://another-url.com")!)
-        let json = makeItemsJSON([item1.json, item2.json])
+        let jsonData = makeItemsJSON([item1.json, item2.json])
 
-        let result = try FeedItemsMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+        let result = try FeedItemsMapper.map(jsonData, from: HTTPURLResponse(statusCode: 200))
 
         XCTAssertEqual(result, [item1.model, item2.model])
     }
 
     // MARK: - Helpers
-
-    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
-        return .failure(error)
-    }
 
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
         let model = FeedImage(id: id, description: description, location: location, URL: imageURL)
@@ -66,16 +62,5 @@ class FeedItemsMapperTests: XCTestCase {
             "image": imageURL.absoluteString
         ].compactMapValues { $0 }
         return (model, json)
-    }
-
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-}
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
 }
