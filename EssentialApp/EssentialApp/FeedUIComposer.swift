2aff6591116996b4db13db02f3243be4dc941cbd
Robert Tanase <robert.tanase97@gmail.com>
2022-06-20 23:15:10 +0300

    Notifies selection handler on image selection

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -17,8 +17,9 @@ public final class FeedUIComposer {
 
     public static func feedComposedWith (
         feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
-        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher
-    ) -> ListViewController {
+        imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
+        selection: @escaping ((FeedImage) -> Void) = { _ in }
+    ) -> ListViewController { 
         let presentationAdapter = FeedPresentationAdapter(loader: { feedLoader().dispatchOnMainQueue() })
         let refreshController = ListRefreshViewController()
         refreshController.onRefresh = presentationAdapter.loadResource
@@ -27,7 +28,8 @@ public final class FeedUIComposer {
         presentationAdapter.presenter = LoadResourcePresenter(
             resourceView: FeedViewAdapter(
                 controller: feedController,
-                imageLoader: imageLoader
+                imageLoader: imageLoader,
+                selection: selection
             ),
             loadingView: WeakRefVirtualProxy(refreshController),
             errorView: WeakRefVirtualProxy(feedController),
--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -61,6 +61,11 @@ final public class ListViewController: UITableViewController, UITableViewDataSou
         dataSource.applySnapshotUsingReloadData(snapshot)
     }
 
+    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let delegate = cellController(at: indexPath)?.delegate
+        delegate?.tableView?(tableView, didSelectRowAt: indexPath)
+    }
+
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let delegate = cellController(at: indexPath)?.delegate
         delegate?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
