fecbb6d4a23fef37da90609b47a9408d11222ebf
Robert Tanase <robert.tanase97@gmail.com>
2022-07-01 19:36:25 +0300

    Add `LoadMore` message label

--- EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -8,20 +8,6 @@
 import UIKit
 
 public class LoadMoreCell: UITableViewCell {
-    private lazy var spinner: UIActivityIndicatorView = {
-        let spinner = UIActivityIndicatorView(style: .medium)
-        contentView.addSubview(spinner)
-
-        spinner.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            contentView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
-        ])
-
-        return spinner
-    }()
-
     public var isLoading: Bool {
         get { spinner.isAnimating }
         set {
@@ -32,4 +18,40 @@ public class LoadMoreCell: UITableViewCell {
             }
         }
     }
+
+    public var message: String? {
+        get { messageLabel.text }
+        set { messageLabel.text = newValue }
+    }
+
+    // MARK: Subviews
+
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+
+        contentView.addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
+        ])
+
+        return spinner
+    }()
+
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .tertiaryLabel
+        label.font = .preferredFont(forTextStyle: .footnote )
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+
+        contentView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.pinToSuperview(constant: 8)
+
+        return label
+    }()
 }
