229e295b9c0964de1dead9c031f6da51a91c30c7
Robert Tanase <robert.tanase97@gmail.com>
2022-07-04 10:40:23 +0300

    Make feed image data cache synchronous

--- EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -16,16 +16,16 @@ public final class LocalFeedImageDataLoader {
 }
 
 extension LocalFeedImageDataLoader: FeedImageDataCache {
-    public typealias SaveResult = FeedImageDataCache.Result
-
     public enum SaveError: Error {
         case failed
     }
 
-    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-        completion(SaveResult {
+    public func save(_ data: Data, for url: URL) throws {
+        do {
             try store.insert(data, for: url)
-        }.mapError { _ in SaveError.failed })
+        } catch {
+            throw SaveError.failed
+        }
     }
 }
 
@@ -59,7 +59,7 @@ extension LocalFeedImageDataLoader: FeedImageDataLoader {
 
     public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
         let task = LoadImageDataTask(completion)
-        
+
         task.complete(
             with: Swift.Result {
                 try store.retrieve(dataForURL: url)
